c8bbc78adb620e8cd1fb8498d5c236ec40c503c5
CureFlora <kyamoonlight@gmail.com>
2021-04-04 15:24:09 +0600

    Add Close Modal

--- js/script.js
+++ js/script.js
@@ -85,8 +85,7 @@ setClock('.timer', deadline);
 /* ---------------------------------- Modal --------------------------------- */
 
 const modalTrigger = document.querySelectorAll('[data-modal]'),
-      modal = document.querySelector('.modal'),
-      modalCloseBtn = document.querySelector('[data-close]');
+      modal = document.querySelector('.modal');
 
 function openModal() {
   modal.classList.add('show');
@@ -103,9 +102,9 @@ function closeModal() {
   modal.classList.remove('show');
   document.body.style.overflow = '';
 }
-modalCloseBtn.addEventListener('click', closeModal);
+
 modal.addEventListener('click', (e) => {
-  if(e.target === modal) {
+  if(e.target === modal || e.target.getAttribute('data-close') == '') {
     closeModal();
   }
 });
@@ -114,14 +113,14 @@ document.addEventListener('keydown', (e) => {
     closeModal();
   }
 });
-// const modalTimerId = setTimeout(openModal, 3000);
-// function showModalByScroll() {
-//   if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-//     openModal();
-//     window.removeEventListener('scroll', showModalByScroll);
-//   }
-// }
-// window.addEventListener('scroll', showModalByScroll);
+const modalTimerId = setTimeout(openModal, 300000);
+function showModalByScroll() {
+  if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+    openModal();
+    window.removeEventListener('scroll', showModalByScroll);
+  }
+}
+window.addEventListener('scroll', showModalByScroll);
 
 
 /* ----------------------- Using Classes for FoodCards ---------------------- */
@@ -195,7 +194,7 @@ new MenuCard(
 const forms = document.querySelectorAll('form');
 
 const message = {
-  loading: 'Загрузка',
+  loading: 'img/form/spinner.svg',
   success: 'Спасибо! Скоро мы с вами свяжемся',
   failure: 'Что то пошло не так...'
 };
@@ -208,10 +207,14 @@ function postData(form) {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const statusMessage = document.createElement('div');
-    statusMessage.classList.add('status');
-    statusMessage.textContent = message.loading;
-    form.append(statusMessage);
+    const statusMessage = document.createElement('img');
+    statusMessage.src = message.loading;
+    statusMessage.style.cssText = `
+      display: block;
+      margin: 0 auto;
+    `;
+    
+    form.insertAdjacentElement('afterend', statusMessage);
 
     const request = new XMLHttpRequest();
     request.open('POST', 'server.php');
@@ -231,15 +234,39 @@ function postData(form) {
     request.addEventListener('load', () => {
       if (request.status === 200) {
         console.log(request.response);
-        statusMessage.textContent = message.success;
+        showThanksModal(message.success);
         form.reset();
-        setTimeout(() => {
-          statusMessage.remove();
-        }, 2000);
+        statusMessage.remove();
       } else {
-        statusMessage.textContent = message.failure;
+        showThanksModal(message.failure);
       }
     });
   });
 }
+
+function showThanksModal(message) {
+  const prevModalDialog = document.querySelector('.modal__dialog');
+
+  prevModalDialog.classList.add('hide');
+  openModal();
+
+  const thanksModal = document.createElement('div');
+  thanksModal.classList.add('modal__dialog');
+  thanksModal.innerHTML = `
+    <div class="modal__content">
+      <div class="modal__close" data-close>x</div>
+      <div class="modal__title">${message}</div>
+    </div>
+  `;
+  document.querySelector('.modal').append(thanksModal);
+  setTimeout(() => {
+    thanksModal.remove();
+    prevModalDialog.classList.add('show');
+    prevModalDialog.classList.remove('hide');
+    closeModal();
+  }, 4000);
+}
+
+
+
 });
