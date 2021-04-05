293f8ee63555c85b5f55d5ee0460dbb547998b54
CureFlora <kyamoonlight@gmail.com>
2021-04-05 09:35:14 +0600

    Modify response function. Adding fetch method

--- js/script.js
+++ js/script.js
@@ -206,20 +206,14 @@ forms.forEach(item => {
 function postData(form) {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-
     const statusMessage = document.createElement('img');
     statusMessage.src = message.loading;
     statusMessage.style.cssText = `
       display: block;
       margin: 0 auto;
     `;
-    
     form.insertAdjacentElement('afterend', statusMessage);
-
-    const request = new XMLHttpRequest();
-    request.open('POST', 'server.php');
-
-    request.setRequestHeader('Content-type', 'application/json');
+    
     const formData = new FormData(form);
 
     const object = {};
@@ -227,19 +221,24 @@ function postData(form) {
       object[key] = value;
     });
     
-    const json = JSON.stringify(object);
 
-    request.send(json);
 
-    request.addEventListener('load', () => {
-      if (request.status === 200) {
-        console.log(request.response);
+    fetch('server.php', {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(object)
+    })
+    .then(data => data.text())
+    .then(data => {
+        console.log(data);
         showThanksModal(message.success);
-        form.reset();
         statusMessage.remove();
-      } else {
-        showThanksModal(message.failure);
-      }
+    }).catch(() => {
+      showThanksModal(message.failure);
+    }).finally(() => {
+      form.reset();
     });
   });
 }
