c38e178580abbfd1b842be07d2d447ee837b37cb
CureFlora <kyamoonlight@gmail.com>
2021-04-07 16:54:59 +0600

    Adding regular expressions

--- js/script.js
+++ js/script.js
@@ -364,11 +364,15 @@ for (let i = 0; i < slides.length; i++) {
   dots.push(dot);
 }
 
+function deleteNotDigits(str) {
+  return +str.replace(/\D/g, '');
+}
+
 next.addEventListener('click', () => {
-  if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+  if (offset == deleteNotDigits(width) * (slides.length - 1)){
     offset = 0;
   } else{
-    offset += +width.slice(0, width.length - 2);
+    offset += deleteNotDigits(width);
   }
 
   slidesField.style.transform = `translateX(-${offset}px)`;
@@ -391,9 +395,9 @@ next.addEventListener('click', () => {
 
 prev.addEventListener('click', () => {
   if (offset == 0){
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = deleteNotDigits(width) * (slides.length - 1);
   } else{
-      offset -= +width.slice(0, width.length - 2);
+      offset -= deleteNotDigits(width);
   }
 
   slidesField.style.transform = `translateX(-${offset}px)`;
@@ -419,7 +423,7 @@ dots.forEach(dot => {
     const slideTo = e.target.getAttribute('data-slide-to');
 
     slideIndex = slideTo;
-    offset = offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+    offset = offset = deleteNotDigits(width) * (slideTo - 1);
 
     slidesField.style.transform = `translateX(-${offset}px)`;
 
